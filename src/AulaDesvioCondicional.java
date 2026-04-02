9b890aad02bb17575c6bc1f389c4508a586aca7a
Bruna Silva <bruna63466756@alunos.sc.senac.br>
2026-04-02 15:14:09 -0300

    exercicios

--- src/AulaDesvioCondicional.java
+++ src/AulaDesvioCondicional.java
@@ -5,49 +5,76 @@ public class AulaDesvioCondicional {
 	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		//exercicio1();
-		//exercicio2();
-		exercicio3();
+		// exercicio1();
+		// exercicio2();
+		// exercicio3();
+		// exercicio4();
+		exercicio5();
 	}
 
 	static void exercicio1() {
-		
+
 		System.out.print("Digite um numero inteiro: ");
 		int numero = sc.nextInt();
-		
-			if (numero % 2 == 0) {
-				System.out.print("O número é par.");
-			} else { 
-				System.out.print("O numero é imprar.");
-			}
+
+		if (numero % 2 == 0) {
+			System.out.print("O número é par.");
+		} else {
+			System.out.print("O numero é imprar.");
+		}
 	}
-	
+
 	static void exercicio2() {
-		
+
 		System.out.print("Digite um numero: ");
 		int numero1 = sc.nextInt();
 		System.out.print("Digite outro numero: ");
-		int numero2 = sc.nextInt ();
-		
-		if (numero1 > numero2 ) {
+		int numero2 = sc.nextInt();
+
+		if (numero1 > numero2) {
 			System.out.print("O número maior é: " + numero1);
 		} else {
 			System.out.print("O número maior é: " + numero2);
 		}
 	}
-	
+
 	static void exercicio3() {
-		
+
 		System.out.print("Digite um número: ");
 		int x = sc.nextInt();
-		
-		if (x > 0 ) {
-		 System.out.print("Esse número é positivo.");
-		} else if  (x < 0) {
+
+		if (x > 0) {
+			System.out.print("Esse número é positivo.");
+		} else if (x < 0) {
 			System.out.print("Esse numero é negativo.");
 		} else {
 			System.out.print("Esse número é igual a 0.");
 		}
+	}
+
+	static void exercicio4() {
+
+		System.out.print("Qual é a sua nota? ");
+		double nota = sc.nextDouble();
+
+		if (nota >= 6) {
+			System.out.printf("Aprovado!");
+		} else {
+			System.out.printf("Reprovado!");
 		}
 	}
 
+	static void exercicio5() {
+
+		System.out.print("Qual a sua idade? ");
+		int idade = sc.nextInt();
+
+		if (idade >= 16) {
+			System.out.println("Você pode votar!");
+		} else {
+			System.out.println("Não pode votar!");
+		}
+
+	}vamos 
+
+}
